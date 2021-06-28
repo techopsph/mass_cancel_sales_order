ece8517a002ff90d461c8a2577477eaa19272849
EL Abquina <eladio@pantheon.io>
2021-06-28 12:42:49 +0800

    Branch 13.0

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Mass Cancel Sales Order',
+    'summary': "Mass Cancel Sales Order",
+    'description': "Mass Cancel Sales Order",
+
+    'author': 'iPredict IT Solutions Pvt. Ltd.',
+    'website': 'http://ipredictitsolutions.com',
+    'support': 'ipredictitsolutions@gmail.com',
+
+    'category': 'Sales',
+    'version': '13.0.0.1.0',
+    'depends': ['sale_management'],
+
+    'data': [
+        'wizard/sale_order_cancel.xml',
+    ],
+
+    'auto_install': False,
+    'installable': True,
+
+    'images': ['static/description/banner.png'],
+}
